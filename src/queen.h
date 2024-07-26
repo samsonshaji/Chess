63c98ef90044528559873ec53dfedb5bae0bbca9
snviswan <snviswan@uwaterloo.ca>
2024-07-26 14:01:12 -0400

    castling logic for rook

--- src/queen.h
+++ src/queen.h
@@ -4,7 +4,6 @@
 #include "piece.h"
 #include "move.h"
 #include "board.h"
-#include <vector>
 
 class Queen : public Piece {
 public:
--- src/rook.cc
+++ src/rook.cc
@@ -59,3 +59,55 @@ std::vector<Move> Rook::getValidMoves() const {
 
     return validMoves;
 }
+
+//check if the rook can castle, assumes king has not moved
+bool Rook::canCastle() const {
+    if (this->getHasMoved()) {
+        return false;
+    } 
+    else {
+        // check for both black and white rooks, both sides, king side - queen side castling
+
+        // white rook
+        if (colour == Colour::White) {
+            // left rook
+            if (square->getX() == 0) {
+                if (board->getSquare(1, 0)->getPiece() == nullptr && board->getSquare(2, 0)->getPiece() == nullptr && board->getSquare(3, 0)->getPiece() == nullptr) {
+                    if (board->getSquare(0, 0)->getPiece() != nullptr && board->getSquare(0, 0)->getPiece()->getHasMoved() == false) {
+                        return true;
+                    }
+                }
+            }
+
+            // right rook
+            else if (square->getX() == 7) {
+                if (board->getSquare(5, 0)->getPiece() == nullptr && board->getSquare(6, 0)->getPiece() == nullptr) {
+                    if (board->getSquare(7, 0)->getPiece() != nullptr && board->getSquare(7, 0)->getPiece()->getHasMoved() == false) {
+                        return true;
+                    }
+                }
+            }
+        } 
+
+        // black rook
+        else {
+            // left rook
+            if (square->getX() == 0) {
+                if (board->getSquare(1, 7)->getPiece() == nullptr && board->getSquare(2, 7)->getPiece() == nullptr && board->getSquare(3, 7)->getPiece() == nullptr) {
+                    if (board->getSquare(0, 7)->getPiece() != nullptr && board->getSquare(0, 7)->getPiece()->getHasMoved() == false) {
+                        return true;
+                    }
+                }
+            } 
+
+            // right rook
+            else if (square->getX() == 7) {
+                if (board->getSquare(5, 7)->getPiece() == nullptr && board->getSquare(6, 7)->getPiece() == nullptr) {
+                    if (board->getSquare(7, 7)->getPiece() != nullptr && board->getSquare(7, 7)->getPiece()->getHasMoved() == false) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+}
