2112b5870e124b43dbc1b8d182478b8a3bfbc7cd
parshjp <pjparikh1023@gmail.com>
2024-07-26 14:28:44 -0400

    knight comments

--- src/knight.cc
+++ src/knight.cc
@@ -16,24 +16,34 @@ Knight::Knight(Colour c) : Piece(c) {
 vector<Move> Knight::getValidMoves() const {
     vector<Move> validMoves;
     
+    //current position of the knight
     int x = square->getX();
     int y = square->getY();
 
+    //destination position of the knight
     int jumpx;
     int jumpy;
 
+    //8 total moves a knight can make
+
+    //check bounds for each move to make sure it is within the board
     if (y + 2 < 8) {
         jumpy = y + 2;
+        //check bounds for each move to make sure it is within the board
         if (x + 1 < 8) {
             jumpx = x + 1;
+            //get the square at the destination position
             Square* jump = board->getSquare(jumpx, jumpy);
+            //if the square is empty, add a normal move to the vector
             if (jump->getPiece() == nullptr) {
                 validMoves.push_back(Move(square, jump, MoveType::Normal));
             }
+            //if the square is occupied by an enemy piece, add a capture move to the vector
             else if (jump->getPiece()->getColour() != colour) {
                 validMoves.push_back(Move(square, jump, MoveType::Capture));
             }
         }
+        //check bounds for each move to make sure it is within the board
         if (x - 1 >= 0) {
             jumpx = x - 1;
             Square* jump = board->getSquare(jumpx, jumpy);
